d4ba8f892ec490cac3364610dde28ae1059fbe38
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-09-06 09:51:13 -0400

    Fixing typo
    Fixing typo

--- matisse-common-vendor.mk
+++ matisse-common-vendor.mk
@@ -29,8 +29,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    vendor/samsung/matisse-common/proprietary/lib/libacdbmapper.so:vendor/lib/libacdbmapper.so \
-	vendor/samsung/matisse-common/proprietary/lib/libaudioalsa.so:vendor/lib/libaudioalsa.so \
+    vendor/samsung/matisse-common/proprietary/lib/libacdbmapper.so:system/vendor/lib/libacdbmapper.so \
+	vendor/samsung/matisse-common/proprietary/lib/libaudioalsa.so:system/vendor/lib/libaudioalsa.so \
 	vendor/samsung/matisse-common/proprietary/etc/acdbdata/STOCK/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
 	vendor/samsung/matisse-common/proprietary/etc/acdbdata/STOCK/General_cal.acdb:system/etc/General_cal.acdb \
 	vendor/samsung/matisse-common/proprietary/etc/acdbdata/STOCK/Global_cal.acdb:system/etc/Global_cal.acdb \
@@ -38,9 +38,9 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/matisse-common/proprietary/etc/acdbdata/STOCK/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
 	vendor/samsung/matisse-common/proprietary/etc/acdbdata/STOCK/Headset_cal.acdb:system/etc/Headset_cal.acdb \
 	vendor/samsung/matisse-common/proprietary/etc/acdbdata/STOCK/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
-	vendor/samsung/matisse-common/proprietary/lib/soundfx/libqcvirt.so:vendor/lib/soundfx/libqcvirt.so \
-	vendor/samsung/matisse-common/proprietary/lib/soundfx/libqcreverb.so:vendor/lib/soundfx/libqcreverb.so \
-	vendor/samsung/matisse-common/proprietary/lib/soundfx/libqcbassboost.so:vendor/lib/soundfx/libqcbassboost.so \
+	vendor/samsung/matisse-common/proprietary/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so \
+	vendor/samsung/matisse-common/proprietary/lib/soundfx/libqcreverb.so:system/vendor/lib/soundfx/libqcreverb.so \
+	vendor/samsung/matisse-common/proprietary/lib/soundfx/libqcbassboost.so:system/vendor/lib/soundfx/libqcbassboost.so \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libacdbloader.so:system/vendor/lib/libacdbloader.so \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libacdbrtac.so:system/vendor/lib/libacdbrtac.so \
     vendor/samsung/matisse-common/proprietary/vendor/lib/libadiertac.so:system/vendor/lib/libadiertac.so \
